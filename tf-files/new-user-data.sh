077ef5cefeed598cd87a25fe38957e11ecdbb1a8
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-02-18 19:15:18 +0100

    new userdate

---
+++ tf-files/new-user-data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo apt install nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+echo "<html><body><h1>Hello, User!</h1></body></html>" | sudo tee /var/www/html/index.html
