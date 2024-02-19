6b1e9970e1a487770009d3703a4bab4c4ec6c0fa
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-02-19 14:44:54 +0100

    new user data

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -6,6 +6,7 @@ cd /home/ubuntu/
 TOKEN=${user-data-git-token}
 git clone https://$TOKEN@github.com/MTalhaKumcu/awsproje2.git
 cd /home/ubuntu/awsproje2
+sudo python3 -m pip install Django
 sudo apt-get install python3 python3-pip python3-venv -y
 sudo apt-get install python3.10-dev default-libmysqlclient-dev -y
 sudo python3 -m venv venv
@@ -14,7 +15,7 @@ source venv/bin/activate
 #cd /home/ubuntu/Strategy-to-improve-Cloud-Failover-Scenario-with-AWS
 sudo pip3 install -r requirements.txt
 cd /home/ubuntu/awsproje2/src
-#python3 -m pip install Django
+sudo python3 -m pip install Django
 #fatih hoca
 #sudo sed -i "s/'database_name'/'${rds_db_name}'/g" settings.py
 #sudo sed -i "s/'user_name'/'${db_username}'/g" settings.py
