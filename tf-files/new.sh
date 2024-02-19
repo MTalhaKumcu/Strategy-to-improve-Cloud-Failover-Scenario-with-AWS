cca76dde558a37d522314005e0199c88e1708685
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-02-19 16:27:36 +0100

    minor changes in blog

--- tf-files/new.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
-echo "<html><body><h1>Hello, User!</h1></body></html>" | sudo tee /var/www/html/index.html
